263b2d58af643b7b80277344c71641059a505654
omeralpi <omeralpi@icloud.com>
2025-06-21 21:27:56 +0300

    feat: add seed data for users and posts in the database

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -8,7 +8,16 @@ import * as schema from "./schema"
 async function main() {
   await reset(db, schema)
 
-  // TODO: Add seed data
+  const [user] = await db.insert(schema.users).values({
+    walletAddress: "GABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  }).returning()
+
+  await db.insert(schema.posts).values({
+    title: "Hello World",
+    content: "You can find video version of this article here. In the previous article, we covered the key aspects of Swift Concurrency to build a solid mental model of it. In this article, we will discuss You can find video version of this article here. In the previous article, we covered the key aspects of Swift Concurrency to build a solid mental model of it. In this article, we will discuss",
+    cover: 'https://files.risein.com/bootcamps/hack-pera/xok4c-cohorts-coverpng',
+    userId: user.id,
+  });
 }
 
 main()
